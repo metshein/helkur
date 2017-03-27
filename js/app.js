dec739e3be194ab47587dca2c06efd865def3401
metshein <metshein@gmail.com>
2017-03-28 02:57:06 +0300

    push

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ var stageWidth = 0;
 var stageHeight = 0;
 var at = 250;
 var cView = 0;
+var roll = "";
+var s = 0;
 
 //Vaikimisi suurused
 var origSizes = new Array();
@@ -20,7 +22,13 @@ $(document).ready(function() {
 });
 
 function init() {
-    $('#valiRoll').css('visibility','visible');
+
+    show("valiRoll", 250, 400);
+    show("roadLowBeam", 250, 400);
+   // hide("roadHighBeam", 250, 400);
+ /*   $('#valiRoll').css('visibility','visible');
+    $('#roadLowBeam').css('visibility','hidden');*/
+    $('#roadHighBeam').css('visibility','hidden');
 };
 
 
@@ -65,6 +73,16 @@ function resizeHandler() {
 		//console.log("MasterScale: "+masterScale);
 	}
 
+    if(isPortrait) {
+		$("#titleStart").css("font-size", masterScale*25 + "px");
+		setPos("titleStart", 0.5*( $("#valiRoll").width() - $("#titleStart").width() ), masterScale * 105 );
+
+
+		setFont("txtStart", masterScale*18);
+		setSize("txtStart", stageWidth - masterScale*58);
+		setPos("txtStart", 0.5*( stageWidth - $("#txtStart").width() ), masterScale * 152 );
+    }
+
 
 	//console.log(stageWidth+"x"+stageHeight);
 	setScale("dashboardImage", masterScale);
@@ -80,110 +98,96 @@ function resizeHandler() {
     //tekstid
     setSize("valiRoll", stageWidth, stageHeight);
 	setPos("valiRoll", 0, 0 );
-    	$("#titleStart").css("font-size", masterScale*30 + "px");
-	setPos("titleStart", 0.5*( $("#valiRoll").width() - $("#titleStart").width() ), masterScale * ( yOffset + 170 ) );
+
+
+    $("#titleStart").css("font-size", masterScale*30 + "px");
+	setPos("titleStart", 0.5*( $("#valiRoll").width() - $("#titleStart").width()), masterScale * ( yOffset + 170 ) );
+
+
+    setSize("txtStart", masterScale*520);
+	setPos("txtStart", 0.5*( stageWidth - $("#txtStart").width() ), masterScale * ( 240 + yOffset ) );
+
+    setPos("pedestrian", 0.5*(stageWidth - masterScale*s*80), masterScale*167 + masterScale*yOffset - (1-s)*masterScale*162 + (1-s)*masterScale*192);
 
 }
 
 function showView( _nView ) {
-	if( _nView == 2 && cView == 2 ) {
-		_nView = 3;
-	}
-	if( _nView != cView ) {
-		if(cView == 1) {
-			hide("viewStart", 250);
-			hide("btnStart", 250);
+    if(_nView){
+        $('#valiRoll').css('visibility','hidden');
+    }
+    if(_nView == 1){
+        //console.log("mees");
+        roll = "mees";
+        show("mees", 250, 250);
 
-		} else if(cView == 2) {
-			hide("viewInfo", 250);
-			hide("btnCloseInfo", 250);
+    }
+    if(_nView == 2){
+        roll = "naine";
+         show("naine", 250, 250);
+    }
+    if(_nView == 3){
+        roll = "laps";
+         show("laps", 250, 250);
+    }
 
-			$("#btnSlider").delay(150).animate( { top: -masterScale*10 + ( (240 - distance) / 220 ) * ( $("#bgSlider").height() - masterScale*20 ) }, 250);
-			$("#btnSliderShadow").delay(150).animate( { top: -masterScale*10 + ( (240 - distance) / 220 ) * ( $("#bgSlider").height() - masterScale*20 ) }, 250);
-			$("#txtDistance").delay(150).animate( { top: -masterScale*10 + ( (240 - distance) / 220 ) * ( $("#bgSlider").height() - masterScale*20 ) + masterScale*6 }, 250);
-
-
-		} else if(cView == 3) {
-			if(beamType == 1) {
-				hide("roadLowBeam", 250, 0, 0.3);
-			} else {
-				hide("roadHighBeam", 250, 0, 0.3);
-			}
-			hide("pedestrian", 250, 0);
-		}
-
-		if(_nView == 2) {
-			show("viewInfo", 250, 250);
-			show("btnCloseInfo", 250, 250);
-			show("iconInfoW", 250);
-
-			$("#drag").draggable("disable");
-
-			if(distance != 20) {
-				$("#btnSlider").animate( { top: -masterScale*10 + ( (240 - 20) / 220 ) * ( $("#bgSlider").height() - masterScale*20 ) }, 250);
-				$("#btnSliderShadow").animate( { top: -masterScale*10 + ( (240 - 20) / 220 ) * ( $("#bgSlider").height() - masterScale*20 ) }, 250);
-				$("#txtDistance").animate( { top: -masterScale*10 + ( (240 - 20) / 220 ) * ( $("#bgSlider").height() - masterScale*20 ) + masterScale*6 }, 250);
-			}
-
-			if(cView == 1) {
-				show("slider", 250, 250);
-				show("btnSlider", 250, 250);
-				show("btnInfo", 250, 275);
-				show("btnLowBeam", 250, 300);
-				show("btnHighBeam", 250, 325);
-				show("btnPlain", 250, 350);
-				show("btnBands", 250, 375);
-				show("btnVest", 250, 400);
-			}
-		}
-		if(_nView == 3) {
-			if(!isHints) {
-				isHints = true;
-				setTimeout(showNextHint, 5000);
-			}
-			cView = _nView;
-			setBeam(beamType);
-			setReflector(reflectorType);
-
-			hide("iconInfoW", 250);
-			//show("iconInfo", 250);
-
-			$("#drag").draggable("enable");
-
-			if(beamType == 1) {
-				show("roadLowBeam", 250, 150, 1, 0.3);
-			} else {
-				show("roadHighBeam", 250, 150, 1, 0.3);
-			}
-			updatePedestrian();
-			show("pedestrian", 250, 250);
-		}
-		cView = _nView;
-	}
+    if(roll){
+         console.log(roll);
+       // $('#roadLowBeam').css('visibility','hidden');
+        show("roadLowBeam", 250, 300);
+        show("pedestrian", 250, 250);
+    }
 }
 
-//Arvutused
+//***************** Abistavad arvutused******************//
+//Määrab laiuse ja kõrguse
 function setSize(_id, _w, _h) {
 	if(_id != undefined) {
 		$("#"+_id).css("width", _w + "px");
 		if(_h != undefined) {
 			$("#"+_id).css("height", _h + "px");
-		}
-	}
-}
+}}}
 
+//Skaleerib
 function setScale(_id, _s) {
 	if(_id != undefined) {
 		var _t = origSizes[_id].split("x");
 		$("#"+_id).css("width", _s * _t[0] + "px");
 		$("#"+_id).css("height", _s * _t[1] + "px");
-		//console.log(_t[0]+" ja "+_t[1])
-	}
-}
+}}
 
+//Määrab asukoha
 function setPos(_id, _x, _y) {
 	if(_id != undefined) {
 		$("#"+_id).css("left", _x + "px");
 		$("#"+_id).css("top", _y + "px");
+}}
+
+//Määrab teksti suuruse
+function setFont( _id, _s) {
+	$("#" + _id).css("font-size", _s + "px");
+}
+
+//Kuvab animeeritult
+function show(_id, _t, _d, _o, _os) {
+	if(_id != undefined) {
+		$("#" + _id).stop();
+		if(_t == undefined) {_t = 1;}
+		if(_d == undefined) {_d = 0;}
+		if(_o == undefined) {_o = 1;}
+		if(_os == undefined) {_os = 0;}
+		$("#" + _id).css("visibility", "visible");
+		$("#" + _id).css("opacity", _os);
+		$("#" + _id).delay(_d).animate( { opacity: _o }, _t );
+	}
+}
+
+//Peidab animeeritult
+function hide(_id, _t, _d, _o) {
+	if(_id != undefined) {
+		$("#" + _id).stop();
+		if(_t == undefined) {_t = 1;}
+		if(_d == undefined) {_d = 0;}
+		if(_o == undefined) {_o = 0;}
+		$("#" + _id).delay(_d).animate( { opacity: _o }, _t, function() { if( _o == 0) { $("#" + _id).css("visibility", "hidden"); } } );
 	}
 }
